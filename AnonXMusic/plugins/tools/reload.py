b2c6fa16d558ffab30767905284965403a840d2c
Asaaq-ali <148828173+Asaaq-ali@users.noreply.github.com>
2023-10-24 22:38:39 +0300

    Update reload.py

--- AnonXMusic/plugins/tools/reload.py
+++ AnonXMusic/plugins/tools/reload.py
@@ -17,7 +17,7 @@ rel = {}
 
 
 @app.on_message(
-    filters.command(["admincache", "reload", "refresh"]) & filters.group & ~BANNED_USERS
+    filters.command(["ريلود", "reload", "refresh"],"") & filters.group & ~BANNED_USERS
 )
 @language
 async def reload_admin_cache(client, message: Message, _):
