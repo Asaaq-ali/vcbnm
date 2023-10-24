a8380d3e4663b46709e8f06c6b685ebf04660151
Asaaq-ali <148828173+Asaaq-ali@users.noreply.github.com>
2023-10-24 22:37:05 +0300

    Update Commands2.py

--- AnonXMusic/plugins/play/Commands2.py
+++ AnonXMusic/plugins/play/Commands2.py
@@ -20,7 +20,7 @@ from config.config import OWNER_ID
 from strings import get_command, get_string
 from YukkiMusic import Telegram, YouTube, app
 from YukkiMusic.misc import SUDOERS
-from YukkiMusic.plugins.play.playlist import del_plist_msg
+from YukkiMusic.plugins.play.play import del_plist_msg
 from YukkiMusic.plugins.sudo.sudoers import sudoers_list
 from YukkiMusic.utils.database import (add_served_chat,
                                        add_served_user,
