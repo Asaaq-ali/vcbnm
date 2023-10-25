a89e0095df1d221b127b119dc30c913b51c47741
Asaaq-ali <148828173+Asaaq-ali@users.noreply.github.com>
2023-10-25 16:13:15 +0300

    Update pause.py

--- AnonXMusic/plugins/admins/pause.py
+++ AnonXMusic/plugins/admins/pause.py
@@ -9,7 +9,7 @@ from AnonXMusic.utils.inline import close_markup
 from config import BANNED_USERS
 
 
-@app.on_message(filters.command(["سهى ايقاف مؤقت", "اسكت"],"") & filters.group & ~BANNED_USERS)
+@app.on_message(filters.command(["ايقاف مؤقت", "سهى اسكت"],"") & filters.group & ~BANNED_USERS)
 @AdminRightsCheck
 async def pause_admin(cli, message: Message, _, chat_id):
     if not await is_music_playing(chat_id):
