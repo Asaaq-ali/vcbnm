781aeb7bd0126d66c8b75079b8d53578a3460161
Asaaq-ali <148828173+Asaaq-ali@users.noreply.github.com>
2023-10-25 18:15:11 +0300

    Update admins.py

--- AnonXMusic/assets/helpers/admins.py
+++ AnonXMusic/assets/helpers/admins.py
@@ -1,7 +1,7 @@
 from typing import List
 from pyrogram.types import Chat
-from Music.helpers.get_admins import get as gett
-from Music.helpers.get_admins import set
+from AnonXMusic.assets.helpers.get_admins import get as gett
+from AnonXMusic.assets.helpers.get_admins import set
 async def get_administrators(chat: Chat) -> List[int]:
     get = gett(chat.id)
 
