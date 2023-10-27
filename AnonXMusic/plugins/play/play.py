14afb1822a7ddd941385bb5b11832107d3fce70b
Asaaq-ali <148828173+Asaaq-ali@users.noreply.github.com>
2023-10-27 05:27:44 +0300

    Update play.py

--- AnonXMusic/plugins/play/play.py
+++ AnonXMusic/plugins/play/play.py
@@ -28,7 +28,7 @@ from config import BANNED_USERS, lyrical
 @app.on_message(
     filters.command(
         [
-            "سهى شغلي",
+            "دينا شغلي",
             "شغل",
             "تشغيل",
             "vplay",
