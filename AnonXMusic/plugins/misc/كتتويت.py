16b925ecbc95c6553b9a8e3c05579f558ace5b32
Asaaq-ali <148828173+Asaaq-ali@users.noreply.github.com>
2023-10-28 05:18:36 +0300

    Update كتتويت.py

--- AnonXMusic/plugins/misc/كتتويت.py
+++ AnonXMusic/plugins/misc/كتتويت.py
@@ -7,6 +7,30 @@ from pyrogram.types import (InlineKeyboardButton,CallbackQuery,
 from AnonXMusic import app
 from config import BANNED_USERS
 
+
+@app.on_message(
+  filters.command(
+      [
+         "انمي", 
+         "انميات",    
+       ],""
+  )
+    & filters.group
+    & ~BANNED_USERS
+)
+async def aflamAR(client: Client, message: Message):
+    await message.reply_text(f"""✧<b> اهلين فيك في قسم أنـــمي  دينا</b>\n\n ✧ <a href=https://t.me/Mlze1bot> 𝑺𝒐𝒖𝒓𝒄𝒆 𝒅𝒊𝒏𝒂 </a>   لتصفح الانميات اضغط على زر انميات  """,
+        reply_markup=InlineKeyboardMarkup(
+              [
+
+        [InlineKeyboardButton("أنـــميــات", callback_data="anmie2 " + str(message.from_user.id))],
+        
+        [InlineKeyboardButton("✧ [𝑺𝒐𝒖𝒓𝒄𝒆 𝒅𝒊𝒏𝒂] ", url=f"https://t.me/Mlze1bot")],
+
+       ]
+    ), 
+     disable_web_page_preview=True
+) 
 ###############################################################################
 ##########################################################################انمي القناص###########################
 @Client.on_callback_query(filters.regex("^anmie (\\d+)$"))
@@ -32,7 +56,7 @@ async def anmie2(c: Client, m: CallbackQuery):
         return
     keyboard = InlineKeyboardMarkup(inline_keyboard=[
 
-        [InlineKeyboardButton("هنتر اكس هنتر ", callback_data="hxsoha " + str(m.from_user.id))], 
+        [InlineKeyboardButton("أنمي هنتر القناص", callback_data="hxsoha " + str(m.from_user.id))], 
         
         [InlineKeyboardButton("✧ [𝑺𝒐𝒖𝒓𝒄𝒆 𝒅𝒊𝒏𝒂]", url=f"https://t.me/Mlze1bot")],
 
