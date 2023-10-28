f92e5e7fc69242cc4ad219133bb002ccefb407f8
Asaaq-ali <148828173+Asaaq-ali@users.noreply.github.com>
2023-10-28 03:46:27 +0300

    Update resume.py

--- AnonXMusic/plugins/admins/resume.py
+++ AnonXMusic/plugins/admins/resume.py
@@ -9,7 +9,7 @@ from AnonXMusic.utils.inline import close_markup
 from config import BANNED_USERS
 
 
-@app.on_message(filters.command(["كمل", "سهى كملي"],"") & filters.group & ~BANNED_USERS)
+@app.on_message(filters.command(["كمل", "دينا كملي","واصل"],"") & filters.group & ~BANNED_USERS)
 @AdminRightsCheck
 async def resume_com(cli, message: Message, _, chat_id):
     if await is_music_playing(chat_id):
