7a0465c0509a34a5df1fd3944dd677d9c25b2ff8
vidroasaac <148828173+Asaaq-ali@users.noreply.github.com>
2023-11-01 20:20:52 +0300

    Update pulay.py

--- AnonXMusic/plugins/play/pulay.py
+++ AnonXMusic/plugins/play/pulay.py
@@ -2,7 +2,7 @@ import asyncio
 
 
 import random
-from AnonX import app
+from AnonXMusic import app
 from pyrogram.types import (InlineKeyboardButton,
                             InlineKeyboardMarkup, Message)
 
